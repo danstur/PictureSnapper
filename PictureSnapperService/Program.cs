ac8126364a0f6fff3e18cf0e328af990c4f427ca
Daniel Sturm <d.sturm@outlook.com>
2022-03-23 23:11:38 +0100

    Try integrate generic host

--- PictureSnapperService/Program.cs
+++ PictureSnapperService/Program.cs
@@ -1,8 +1,26 @@
 using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.NamingConventionBinder;
+using Microsoft.Extensions.Hosting;
 using System.CommandLine.Parsing;
+using Microsoft.Extensions.DependencyInjection;
 using System.Runtime.InteropServices;
 
 namespace PictureSnapperService;
+
+public sealed class Foo : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask; 
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
 
 public static class Program
 {
@@ -11,12 +29,20 @@ public static class Program
     [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
     private static extern void GrabImage(out IntPtr image, out int imageSize);
 
-    private static void Execute(DirectoryInfo directory, TimeSpan interval)
+    private static void Run(DirectoryInfo directory, TimeSpan interval)
     {
         Console.WriteLine($"Hello world: {directory.FullName} in interval: {interval}");
     }
 
-    public static unsafe void Main(string[] args)
+    //GrabImage(out var imagePtr, out var imageSize);
+    //using var _ = new CoTaskMemAllocatedPointer(imagePtr);
+
+    //var image = new Span<byte>(imagePtr.ToPointer(), imageSize);
+    //using var fw = File.OpenWrite(@"c:\tmp\test.jpg");
+    //fw.Write(image);
+    //Console.WriteLine("All finished");
+
+    private static CommandLineBuilder BuildCommandLine()
     {
         var options = new Option[]
         {
@@ -47,15 +73,19 @@ public static class Program
         {
             rootCommand.AddOption(option);
         }
-        rootCommand.SetHandler<DirectoryInfo, TimeSpan>(Execute, options);
-        rootCommand.Invoke(args);
-
-        //GrabImage(out var imagePtr, out var imageSize);
-        //using var _ = new CoTaskMemAllocatedPointer(imagePtr);
-
-        //var image = new Span<byte>(imagePtr.ToPointer(), imageSize);
-        //using var fw = File.OpenWrite(@"c:\tmp\test.jpg");
-        //fw.Write(image);
-        //Console.WriteLine("All finished");
+        rootCommand.Handler = CommandHandler.Create<DirectoryInfo, TimeSpan>(Run);
+        return new CommandLineBuilder(rootCommand);
     }
+
+    public static Task Main(string[] args) => BuildCommandLine()
+            .UseHost(_ => Host.CreateDefaultBuilder(), host =>
+            {
+                host.ConfigureServices(services =>
+                {
+                    services.AddHostedService<Foo>();
+                });
+            })
+            .UseDefaults()
+            .Build()
+            .InvokeAsync(args);
 }
